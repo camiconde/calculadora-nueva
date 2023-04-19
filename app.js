f9b5b51a193f435a243c2fcda5086fe64fd9c40c
camila <emacamilaconde@gmail.com>
2023-04-18 21:30:07 -0300

    subiendo cambios

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
 const fs = require ('fs')
+const {list} = require ("./funciones/list")
+const {find} = require ("./funciones/find")
+const {edit} = require ("./funciones/edit")
+
+
+console.log (list())
+console.log (find("sacar el gato"))
+console.log (edit ("sacar basura","nueva descripcion"))
